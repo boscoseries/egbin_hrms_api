eb51d0405163fcead1cbde7c4fc61c9ec8868cf7
Johnbosco <boscoseries@gmail.com>
2021-01-30 20:56:25 +0100

    deploy

--- core/settings.py
+++ core/settings.py
@@ -165,3 +165,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
